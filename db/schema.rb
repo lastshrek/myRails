9cd389f41df44ff5031db9c0a5f40b45e62dd62b
Purchas <lastshrek@foxmail.com>
2016-08-29 14:55:29 +0800

    section 6  complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160829041703) do
+ActiveRecord::Schema.define(version: 20160829043843) do
 
   create_table "microposts", force: :cascade do |t|
     t.string   "content"
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(version: 20160829041703) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

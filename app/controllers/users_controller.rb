578c8fb73e342628ff6994b249dd22a5b6fcaf7b
Purchas <lastshrek@foxmail.com>
2016-09-12 11:02:51 +0800

    1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,11 @@ class UsersController < ApplicationController
 
 	private
 		def user_params
-		  params.require(:user).permit(:name, :email, :password,
-																		:password_confirmation)
+		  params.require(:user).permit(
+																	 :name,
+			 														 :email,
+																	 :password,
+																	 :password_confirmation
+																	)
 		end
 end
